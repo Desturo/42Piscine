afe24112ad91e3d9c8330ba2820a8f6651da8e6b
Nico Schneider <nschneid@1-D-9.42heilbronn.de>
2024-12-03 12:30:07 +0100

    Pushed at 2024-12-03_12-30-00.

--- C07/ex04/ft_convert_base.c
+++ C07/ex04/ft_convert_base.c
@@ -6,7 +6,7 @@
 /*   By: nschneid <nschneid@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 13:49:42 by nschneid          #+#    #+#             */
-/*   Updated: 2024/12/03 10:39:07 by nschneid         ###   ########.fr       */
+/*   Updated: 2024/12/03 12:29:51 by nschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,3 +86,5 @@ int	main(void)
 	printf("%d\n", to_base_ten("f3123", "0123456789ABCDEF"));
 	return (0);
 }
+
+//This is a test
